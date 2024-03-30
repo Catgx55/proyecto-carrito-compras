e74c027592a343c9fdae137628e5374bf7421afd
Cesar Trujillo <catgx55@gmail.com>
2024-03-30 00:05:38 -0500

    agregando props, funcion addToCart.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
+import { db } from "./data/db"
 
 function App() {
 
     //State
-    const [] = useState()
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
+
+    function addToCart(item) {
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        console.log(itemExists)
+        setCart(prevCart => [...prevCart, item])
+    }
 
 return (
     <>
@@ -15,16 +23,14 @@ return (
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
+            {data.map((guitar) => (
+                <Guitar
+                    key={guitar.id}
+                    guitar={guitar}
+                    setCart={setCart}
+                    addToCart={addToCart}
+                />
+            ))}
         </div>
     </main>
 
