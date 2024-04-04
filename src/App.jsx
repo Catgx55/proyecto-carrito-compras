c57b446fd2e8fdbeddf51334ad95746f648e1144
Cesar Trujillo <catgx55@gmail.com>
2024-04-04 00:48:56 -0500

    localStorage, funcion eliminar, decrementar, incrementar y vaciar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
 import { db } from "./data/db"
 
 function App() {
 
+    //funcion persistencia LocalStorage
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
+    }
+
     //State
-    const [data, setData] = useState(db)
-    const [cart, setCart] = useState([])
+    const [data] = useState(db)
+    const [cart, setCart] = useState(initialCart)
+
+    const MAX_ITEM = 5
+    const MIN_ITEM = 1
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
 
     function addToCart(item) {
         const itemExists = cart.findIndex(guitar => guitar.id === item.id)
         if(itemExists >= 0){//existe en el carrito
+            if(cart[itemExists].quantity >= MAX_ITEM) return
             const updateCart = [...cart]//actualizacion de cantidad sin mutar el state
             updateCart[itemExists].quantity++
             setCart(updateCart)
@@ -21,9 +35,53 @@ function App() {
         }
     }
 
+    //funcion eliminar item
+    function removeFromCart(id) {
+        setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+    }
+
+    //funcion incrementar item
+    function increaseQuantity(id) {
+        const updatedCart = cart.map(item => {
+            if(item.id === id && item.quantity < MAX_ITEM){
+                return{
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    //funcion decrementar item
+    function decreaseQuantity(id){
+        const dowCart = cart.map(item => {
+            if(item.id === id && item.quantity > MIN_ITEM){
+                return{
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+            }
+            return item
+        })
+        setCart(dowCart)
+    }
+
+    //funcion vaciar item
+    function clearCart(e) {
+        setCart([])
+    }
+
 return (
     <>
-    <Header />
+    <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+    />
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
